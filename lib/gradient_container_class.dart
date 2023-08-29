b36fab8d0d0a539edd85fc006d9966847b75fe1a
Annemarie <a.lucernoni0@gmail.com>
2023-08-29 11:38:16 -0600

    cleaned up comments from prior branch

--- lib/gradient_container_class.dart
+++ lib/gradient_container_class.dart
@@ -2,51 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:dice_project/text_widget.dart';
 
 class GradientContainer extends StatelessWidget {
-  // const GradientContainer({super.key, required this.colors});
-
-  // final List<Color> colors;
-
-  // @override
-  // Widget build(context) {
-  //   return Container(
-  //     decoration: BoxDecoration(
-  //       gradient: LinearGradient(
-  //         colors: colors,
-  //         begin: Alignment.topLeft,
-  //         end: Alignment.bottomRight,
-  //       ),
-  //     ),
-  //     child: const Center(
-  //       child: TextWidget('hello there!'),
-  //     ),
-  //   );
-  // }
-
-  // const GradientContainer(this.color1, this.color2, {super.key});
-
-  // can make multiple constructor functions, like this one. in main.dart would simply pass body: GradientContainer.purple()
-  // const GradientContainer.purple({super.key})
-  //     : color1 = Color.fromARGB(255, 63, 25, 128),
-  //       color2 = Color.fromARGB(255, 167, 131, 229);
-
-  // final Color color1;
-  // final Color color2;
-
-  // @override
-  // Widget build(context) {
-  //   return Container(
-  //     decoration: BoxDecoration(
-  //       gradient: LinearGradient(
-  //         colors: [color1, color2],
-  //         begin: Alignment.topLeft,
-  //         end: Alignment.bottomRight,
-  //       ),
-  //     ),
-  //     child: const Center(
-  //       child: TextWidget('hello there!'),
-  //     ),
-  //   );
-  // }
 
   GradientContainer(this.color1, this.color2, {super.key});
 
@@ -74,19 +29,15 @@ class GradientContainer extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
-              'assets/images/dice-1.png',
+              currentDiceImage,
               width: 200,
             ),
             const SizedBox(
               height: 20,
             ),
             TextButton(
-                // onPressed: () {
-                //  anonymous function code
-                // },
                 onPressed: rollDice,
                 style: TextButton.styleFrom(
-                  // padding:  const EdgeInsets.only(top: 200),
                   foregroundColor: const Color.fromARGB(255, 172, 199, 173),
                   textStyle: const TextStyle(
                     fontSize: 28,
