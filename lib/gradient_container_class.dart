07a6c4d31ae856b7662b4789c5f76d31a0f86f8f
Annemarie <a.lucernoni0@gmail.com>
2023-08-29 11:14:00 -0600

    added assets folder to contain images and changed gradientcontainer widget to display a dice image and a button

--- lib/gradient_container_class.dart
+++ lib/gradient_container_class.dart
@@ -22,10 +22,40 @@ class GradientContainer extends StatelessWidget {
   //   );
   // }
 
+  // const GradientContainer(this.color1, this.color2, {super.key});
+
+  // can make multiple constructor functions, like this one. in main.dart would simply pass body: GradientContainer.purple()
+  // const GradientContainer.purple({super.key})
+  //     : color1 = Color.fromARGB(255, 63, 25, 128),
+  //       color2 = Color.fromARGB(255, 167, 131, 229);
+
+  // final Color color1;
+  // final Color color2;
+
+  // @override
+  // Widget build(context) {
+  //   return Container(
+  //     decoration: BoxDecoration(
+  //       gradient: LinearGradient(
+  //         colors: [color1, color2],
+  //         begin: Alignment.topLeft,
+  //         end: Alignment.bottomRight,
+  //       ),
+  //     ),
+  //     child: const Center(
+  //       child: TextWidget('hello there!'),
+  //     ),
+  //   );
+  // }
+
   const GradientContainer(this.color1, this.color2, {super.key});
 
   final Color color1;
   final Color color2;
+
+  void rollDice() {
+    // code to roll dice
+  }
 
   @override
   Widget build(context) {
@@ -37,8 +67,21 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: TextWidget('hello there!'),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            TextButton(
+              // onPressed: () {
+              //  anonymous function code
+              // }, 
+              onPressed: rollDice,
+              child: const Text('Roll that bish')),
+          ],
+        ),
       ),
     );
   }
