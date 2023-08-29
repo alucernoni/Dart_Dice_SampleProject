27c1ec106a50f09e0f8ef0b038518d56e7a6119f
Annemarie <a.lucernoni0@gmail.com>
2023-08-29 10:39:37 -0600

    added text variable to text widget so no longer hardcoded

--- lib/text_widget.dart
+++ lib/text_widget.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class TextWidget extends StatelessWidget {
-  const TextWidget({super.key});
+  const TextWidget(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Hello there!',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
